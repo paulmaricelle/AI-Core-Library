02fa42a4b35c687eef00393e9b4771a073c2c972
Paul Maricelle <paul.maricelle@etu.minesparis.psl.eu>
2026-04-01 22:00:15 +0200

    Beginning of LayerNormalization

---
+++ src/ai_lib/layers/layer_normalization.py
@@ -0,0 +1,43 @@
+from .layer import Layer
+import numpy as np
+
+class LayerNormalization(Layer):
+    def __init__(self, n_features, epsilon = 10e-10):
+        super().__init__()
+
+        self.n_features = n_features
+        self.gamma = np.ones((self.n_features, 1))
+        self.beta = np.zeros((self.n_features, 1))
+        self.grad_gamma = np.zeros((self.n_features, 1))
+        self.grad_beta = np.zeros((self.n_features, 1))
+        self.epsilon = epsilon
+        
+
+    def forward(self, X):
+        self.input = X
+        self.mean = np.mean(X, axis=0, keepdims=True)
+        self.var = np.var(X, axis=0, mean=self.mean)
+        self.X_centered = X - self.mean
+        self.std_inv = 1 / (np.sqrt(self.var) + self.epsilon) + self.beta
+        self.X_hat = self.X_centered * self.std_inv
+        return self.gamma * self.X_hat
+    
+    def backward(self, grad_wrt_output):
+        self.grad_gamma = np.sum(grad_wrt_output * self.X_hat, axis=1, keepdims=True)
+        self.grad_beta = np.sum(grad_wrt_output, axis=1, keepdims=True)
+
+        batch_size = self.X.shape[1]
+        n_features = self.X.shape[0]
+
+        #A finir
+        return 
+    
+    def get_params(self):
+        return [self.gamma, self.beta]
+    
+    def get_grads(self):
+        return [self.grad_gamma, self.grad_beta]
+    
+    def zero_grad(self):
+        self.grad_gamma = 0
+        self.grad_beta = 0
