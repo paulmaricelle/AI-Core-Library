e902e2cc9690cdff251d3b7d78e6caadc93d7ed8
Paul Maricelle <paul.maricelle@etu.minesparis.psl.eu>
2026-04-24 11:52:08 +0200

    Fixing a pointless x2 of RAM usage

--- src/ai_lib/dataLoader.py
+++ src/ai_lib/dataLoader.py
@@ -10,18 +10,11 @@ class DataLoader():
         self.n_samples = X.shape[0]
 
     def __iter__(self) -> Iterator[Tuple[np.ndarray, np.ndarray]]:
-        if self.shuffle:
-            permutation = np.random.permutation(self.n_samples)
-            X_iter = self.X[permutation]
-            y_iter = self.y[permutation]
-        else:
-            X_iter = self.X
-            y_iter = self.y
+        indices = np.random.permutation(self.n_samples) if self.shuffle else np.arange(self.n_samples)
 
         for i in range(0, self.n_samples, self.batch_size):
-            x_batch = X_iter[i : i + self.batch_size]
-            y_batch = y_iter[i : i + self.batch_size]
-            yield x_batch, y_batch
+            batch_idx = indices[i: i+self.batch_size]
+            yield self.X[batch_idx], self.Y[batch_idx]
 
     def __len__(self) -> int:
         return (self.n_samples + self.batch_size - 1) // self.batch_size
