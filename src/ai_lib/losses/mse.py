461fa0b257fc2d466f3387c80668725438a2dc57
Paul Maricelle <paul.maricelle@etu.minesparis.psl.eu>
2026-04-24 11:55:43 +0200

    Aligning MSE definition as a metric and as a loss

--- src/ai_lib/losses/mse.py
+++ src/ai_lib/losses/mse.py
@@ -5,11 +5,10 @@ class MSE(Loss):
     def forward(self, y_pred: np.ndarray, y_true: np.ndarray) -> float:
         self.y_pred = y_pred
         self.y_true = y_true
-        batch_size = y_pred.shape[0]
-        return np.sum((y_true-y_pred)**2)/batch_size
+        self.y_size = y_pred.size
+        return float(np.mean((y_true-y_pred)**2))
     
     def backward(self) -> np.ndarray:
-        batch_size = self.y_pred.shape[0]
-        return 2.0 * (self.y_pred - self.y_true)/batch_size
+        return 2.0 * (self.y_pred - self.y_true) / self.y_size
     
 
