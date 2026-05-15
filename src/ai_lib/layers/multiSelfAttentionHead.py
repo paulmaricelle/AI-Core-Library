c00fd9effd2db14df4efbeb5c52efa66c8810d34
Paul Maricelle <paul.maricelle@etu.minesparis.psl.eu>
2026-05-15 11:00:12 +0200

    Adding MultiHeadAttention layer

---
+++ src/ai_lib/layers/multiSelfAttentionHead.py
@@ -0,0 +1,119 @@
+from .layer import Layer
+import numpy as np
+
+class MultiSelfAttentionHead(Layer):
+    def __init__(self, n_heads: int, d_model: int) -> None:
+        """
+        Initializes query, key and value matrices for self-attention mechanism 
+        with n_heads heads each using d_model / n_heads -dimension queries, keys and values
+        """
+        super().__init__()
+        self.n_heads = n_heads
+        self.d_model = d_model
+
+        assert d_model % n_heads == 0
+        self.d_k = d_model // n_heads
+
+        self.W_q = np.random.randn(d_model, d_model) * np.sqrt(2 / d_model)
+        self.W_k = np.random.randn(d_model, d_model) * np.sqrt(2 / d_model)
+        self.W_v = np.random.randn(d_model, d_model) * np.sqrt(2 / d_model)
+
+        self.W_o = np.random.randn(d_model, d_model) * np.sqrt(2 / d_model)
+    
+    def forward(self, X: np.ndarray) -> np.ndarray:
+        """
+        Computes the scaled dot-product attention for an array X (Batch, Sequence length, model dimension (d_model))
+        No implementation of KV cache for now, as it would require me to modify Model, Sequential.
+        """
+        self.X = X
+        B, T, D = X.shape
+
+        Q = np.einsum('bid,dj->bij', X, self.W_q)
+        K = np.einsum('bid,dj->bij', X, self.W_k)
+        V = np.einsum('bid,dj->bij', X, self.W_v)
+
+        # Reshaping for multi-head format (d_model -> (n_heads, d_k))
+
+        Q = Q.reshape(B, T, self.n_heads, self.d_k)
+        K = K.reshape(B, T, self.n_heads, self.d_k)
+        V = V.reshape(B, T, self.n_heads, self.d_k)
+
+        # Shape (B, n_head, T, T)
+        S = np.einsum('btnd,bsnd->bnts', Q, K) / np.sqrt(self.d_k)
+
+        S_max = np.max(S, axis=-1, keepdims=True)
+        exp_S = np.exp(S - S_max) 
+        A = exp_S / np.sum(exp_S, axis=-1, keepdims=True)
+        self.A = A
+
+        Z = np.einsum('bnts,bsnd->btnd', A, V)
+        Z = Z.reshape(B, T, D) # Concatenation of heads
+
+        Out = np.einsum('bid,dj->bij', Z, self.W_o)
+
+        self.Q, self.K, self.V, self.Z = Q,K, V, Z
+        return Out
+    
+    def backward(self, d_output: np.ndarray) -> np.ndarray:
+        """
+        Computes the gradients of the loss with respect to the parameters and to X
+        """
+        B, T, D = self.X.shape
+        self.dW_o = np.einsum('bid,bij->dj', self.Z, d_output)
+
+        dZ_flat = np.einsum('bij,dj->bid', d_output, self.W_o)
+        dZ = dZ_flat.reshape(B, T, self.n_heads, self.d_k)
+
+        # gradients with respect to V and A
+        dV = np.einsum('bhts,bthd->bshd', self.A, dZ)
+        dA = np.einsum('bthd,bshd->bhts', dZ, self.V)
+
+        # Softmax
+        sum_dA_A = np.sum(dA * self.A, axis=-1, keepdims=True)
+        dS = self.A * (dA - sum_dA_A) / np.sqrt(self.d_k)
+
+        # Gradients with respect to Q and K
+        dQ = np.einsum('bhts,bshd->bthd', dS, self.K)
+        dK = np.einsum('bhts,bthd->bshd', dS, self.Q)
+
+        # Back to (B, T, model_d) shape
+        dQ_flat = dQ.reshape(B, T, D)
+        dK_flat = dK.reshape(B, T, D)
+        dV_flat = dV.reshape(B, T, D)
+        
+        # Forward : Q = X * W_q etc
+        self.dW_q = np.einsum('bid,bij->dj', self.X, dQ_flat)
+        self.dW_k = np.einsum('bid,bij->dj', self.X, dK_flat)
+        self.dW_v = np.einsum('bid,bij->dj', self.X, dV_flat)
+
+        # Get gradients with respect to different X and sum them
+        dX_q = np.einsum('bij,dj->bid', dQ_flat, self.W_q)
+        dX_k = np.einsum('bij,dj->bid', dK_flat, self.W_k)
+        dX_v = np.einsum('bij,dj->bid', dV_flat, self.W_v)
+        
+        dX = dX_q + dX_k + dX_v
+        return dX
+
+    def get_params(self):
+        return [self.W_q, self.W_k, self.W_v, self.W_o]
+    
+    def get_grads(self):
+        return [self.dW_q, self.dW_k, self.dW_v, self.dW_o]
+    
+    def get_reg_info(self):
+        return [True, True, True, True]
+    
+    def zero_grad(self):
+        self.dW_q = None
+        self.dW_k = None
+        self.dW_v = None
+        self.dW_o = None
+
+    def get_state(self):
+        return {'W_q' : self.W_q, 'W_k' : self.W_k, 'W_v' : self.W_v, 'W_o' : self.W_o}
+    
+    def set_state(self, state):
+        self.W_q = state["W_q"].copy()
+        self.W_k = state["W_k"].copy()
+        self.W_v = state["W_v"].copy()
+        self.W_o = state["W_o"].copy()
