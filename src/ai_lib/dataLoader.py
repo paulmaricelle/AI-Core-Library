605fb0db942e3decf3bce0198fb8e69e5dc07a43
Paul Maricelle <paul.maricelle@etu.minesparis.psl.eu>
2026-05-15 21:21:42 +0200

    Creating a dataloader for continuous series

--- src/ai_lib/dataLoader.py
+++ src/ai_lib/dataLoader.py
@@ -19,3 +19,31 @@ class DataLoader():
 
     def __len__(self) -> int:
         return (self.n_samples + self.batch_size - 1) // self.batch_size
+    
+class SequenceDataLoader():
+    def __init__(self, data: np.ndarray, block_size:int, batch_size:int, steps_per_epoch:int):
+        """
+        Specific DataLoader for continuous series (NLP / Time series), loading random sliding windows.
+        """
+
+        self.data = data
+        self.block_size = block_size
+        self.batch_size = batch_size
+        self.steps_per_epoch = steps_per_epoch
+        self.n_samples = steps_per_epoch * batch_size
+
+    def __iter__(self):
+        self.current_step = 0
+        return self
+
+    def __next__(self):
+        if self.current_step >= self.steps_per_epoch:
+            raise StopIteration
+        
+        ix = np.random.randint(0, len(self.data) - self.block_size, size=self.batch_size)
+
+        X = np.stack([self.data[i : i + self.block_size] for i in ix])
+        Y = np.stack([self.data[i + 1 : i + self.block_size + 1] for i in ix])
+        
+        self.current_step += 1
+        return X, Y
