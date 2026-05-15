e71d7e2268ecc5102a51c1a0079381697364d9b1
Paul Maricelle <paul.maricelle@etu.minesparis.psl.eu>
2026-05-15 21:09:42 +0200

    Making SoftmaxCrossEntropy compatible with LLM outputs

--- src/ai_lib/losses/softmax_cross_entropy.py
+++ src/ai_lib/losses/softmax_cross_entropy.py
@@ -5,19 +5,39 @@ import numpy as np
 #It eases the backpropagation
 class SoftmaxCrossEntropy(Loss):
     def forward(self, X: np.ndarray, y_true: np.ndarray) -> float:
-        self.y_true = y_true
-
-        exp = np.exp(X - np.max(X, axis=1, keepdims=True))
-        softmax = exp/np.sum(exp, axis=1, keepdims=True )
-        self.y_pred = softmax
-
-        batch_size = X.shape[0]
-        log_prob = -np.log(softmax + 1e-15)
-
-        loss = np.sum(log_prob * y_true) / batch_size
+        """
+        Supports classic format (Batch, Classes) as well as NLP format (Batch, Time, Vocab)
+        """
+        self.original_X_shape = X.shape
+        
+        X_flat = X.reshape(-1, X.shape[-1])
+        
+        
+        if y_true.shape == X.shape:
+            # If class is one hot encoded
+            self.y_flat = np.argmax(y_true.reshape(-1, X.shape[-1]), axis=1)
+        else:
+            # If class is directly predicted with its index
+            self.y_flat = y_true.reshape(-1)
+            
+        self.N = X_flat.shape[0]
+        
+        exp = np.exp(X_flat - np.max(X_flat, axis=1, keepdims=True))
+        self.softmax = exp / np.sum(exp, axis=1, keepdims=True)
+        
+        correct_class_probs = self.softmax[np.arange(self.N), self.y_flat]
+        
+        log_prob = -np.log(correct_class_probs + 1e-15)
+        loss = np.sum(log_prob) / self.N
+        
         return loss
     
     def backward(self) -> np.ndarray:
-        batchsize = self.y_true.shape[0]
-        return (self.y_pred - self.y_true) / batchsize
+        dX_flat = self.softmax.copy()
+        
+        # Gradient of the SoftmaxCrossEntropy is predictioin - truth
+        dX_flat[np.arange(self.N), self.y_flat] -= 1
+
+        dX_flat = dX_flat / self.N
+        return dX_flat.reshape(self.original_X_shape)
 
