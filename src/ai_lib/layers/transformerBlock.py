b9a26f7a7e3d48184d27af30e5bf52937c92aaf2
Paul Maricelle <paul.maricelle@etu.minesparis.psl.eu>
2026-05-15 11:34:38 +0200

    Adding the TransformerBlock as a layer

---
+++ src/ai_lib/layers/transformerBlock.py
@@ -0,0 +1,47 @@
+from .layer import Layer
+from .layer_normalization import LayerNormalization
+from .linear import Linear
+from .residual_block import ResidualBlock
+from .multiSelfAttentionHead import MultiSelfAttentionHead
+from .activations import ReLU
+from ..sequential import Sequential
+
+import numpy as np
+
+
+class TransformerBlock(Layer):
+    def __init__(self, n_heads: int, d_model: int, d_ff: int) -> None:
+        """
+        Initializes a TransformerBlock with pre-normalization :
+        ResidualBlock( _, MultiSelfHeadAttention(LayerNorm())) -> LayerNorm -> ResidualBlock(FFN with d_ff-dimension hidden layer)
+        """
+        super().__init__()
+
+        ln1 = LayerNormalization(d_model)
+        mha = MultiSelfAttentionHead(n_heads=n_heads, d_model=d_model)
+
+        ln2 = LayerNormalization(d_model)
+        
+        res_mha = ResidualBlock([ln1, mha])
+        res_ffn = ResidualBlock([ln2, Linear(d_model, d_ff), ReLU(), Linear(d_ff, d_model)])
+
+        self.seq = Sequential([res_mha, ln2, res_ffn])
+
+    def forward(self, X: np.ndarray) -> np.ndarray:
+        return self.seq.forward(X)
+    
+    def backward(self, dOutput: np.ndarray) -> np.ndarray:
+        return self.seq.backward(dOutput)
+    
+    def get_params(self) -> list:
+        return self.seq.get_params()
+
+    def get_grads(self) -> list:
+        return self.seq.get_grads()
+
+    def get_state(self) -> dict:
+        return self.seq.get_state()
+
+    def set_state(self, state: dict) -> None:
+        self.seq.set_state(state)
+    
