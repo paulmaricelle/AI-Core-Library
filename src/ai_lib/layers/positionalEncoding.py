7fc83fb79c02a920f23016262ba34a6e7f4a34f0
Paul Maricelle <paul.maricelle@etu.minesparis.psl.eu>
2026-05-16 11:13:07 +0200

    Implementing KV_cache in the encoder

--- src/ai_lib/layers/positionalEncoding.py
+++ src/ai_lib/layers/positionalEncoding.py
@@ -5,6 +5,10 @@ class SineCosEncoder(Layer):
     def __init__(self, d_model: int, max_len: int) -> None:
         super().__init__()
         self.d_model = d_model
+        self.max_len = max_len
+
+        self.use_cache = False
+        self.current_pos = 0
         
         pe = np.zeros((max_len, d_model))
         position = np.arange(0, max_len)[:, np.newaxis]
@@ -18,8 +22,21 @@ class SineCosEncoder(Layer):
 
     def forward(self, X: np.ndarray) -> np.ndarray:
         T = X.shape[1]
-        return X + self.pe[:, :T, :]
-    
+
+        if self.use_cache:
+            pos_start = self.current_pos
+            pos_end = self.current_pos + T
+            if pos_end > self.max_len:
+                raise ValueError(f"Generated text exceeds max length for positional encoder :({self.max_len}).")
+
+
+            out = X + self.pe[:, pos_start:pos_end, :]
+            self.current_pos += T
+
+            return out 
+        else:
+            return X + self.pe[:, :T, :]
+
     def backward(self, dX: np.ndarray) -> np.ndarray:
         return dX
     
@@ -29,3 +46,9 @@ class SineCosEncoder(Layer):
     def zero_grad(self): pass
     def get_state(self): return {}
     def set_state(self, state): pass
+
+    def set_use_cache(self, use_cache: bool) -> None:
+        self.use_cache = use_cache
+        
+    def reset_cache(self) -> None:
+        self.current_pos = 0
