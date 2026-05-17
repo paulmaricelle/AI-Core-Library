454dad29505750b11fa05f43cdf45c0643b3eaa6
Paul Maricelle <paul.maricelle@etu.minesparis.psl.eu>
2026-05-17 09:28:24 +0200

    Adding BPETokenizer using tiktoken

--- src/ai_lib/preprocessing/tokenizers.py
+++ src/ai_lib/preprocessing/tokenizers.py
@@ -16,3 +16,38 @@ class CharTokenizer:
         if isinstance(tokens, int) or isinstance(tokens, np.integer):
             tokens = [tokens]
         return "".join([self.idx_to_char[i] for i in tokens])
+    
+import tiktoken
+class FilteredBPETokenizer:
+    def __init__(self, corpus: str, encoding_name = "gpt2"):
+        """
+        BPE Tokenizer based on gpt2's vocabulary though only filtered to use the tokens that appear in the corpus
+        Uses tiktotken. This is my first implementation which is not from scratch (except for numpy ofc).
+        This is because using a BPE tokenizer should be a huge leap in performance, and implementing an optimized
+        version of the algorithm would take too long
+        """
+
+        self.base_tokenizer = tiktoken.get_encoding(encoding_name)
+
+        full_encoded = self.base_tokenizer.encode(corpus)
+
+        unique_tokens = sorted(list(set(full_encoded)))
+        self.vocab_size = len(unique_tokens)
+
+        print(f"Original GPT-2 vocab size : {self.base_tokenizer.n_vocab} versus used vocab size : {self.vocab_size}")
+
+        # Encoding
+        self.old_to_new = {old_id: new_id for new_id, old_id in enumerate(unique_tokens)}
+
+        # Decoding
+        self.new_to_old = {new_id: old_id for new_id, old_id in enumerate(unique_tokens)}
+
+    def encode(self, text: str) -> list:
+        raw_tokens = self.base_tokenizer.encode(text)
+        # Tokens outside of vocab are ignored without raising anything
+        safe_tokens = [self.old_to_new[t] for t in raw_tokens if t in self.old_to_new]
+        return safe_tokens
+    
+    def decode(self, tokens: list) -> list:
+        raw_tokens = [self.new_to_old[t] for t in tokens]
+        return self.base_tokenizer.decode(raw_tokens)
